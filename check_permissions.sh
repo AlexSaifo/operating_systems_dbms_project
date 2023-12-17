ef6997a88aaba7e6985115de03aa1d46b8b9175b
Alaa Kaadan <xxxyyyzzz795@gmail.com>
2023-12-17 16:28:19 +0300

    add check_permissions file

---
+++ check_permissions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+database_name=$1
+
+if [ $1 = "-h" ]; then
+    echo "Usage $0 [DATABASE_NAME]"
+    exit 0
+fi
+
+if [ ! -d "databases/${database_name}" ] || [ ! $2 "databases/${database_name}" ] || [ -z $database_name ]
+then
+    echo "Permession Denied"
+    exit 1
+fi
+exit 0
