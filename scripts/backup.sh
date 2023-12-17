25c7e9d7ca6366b61d072604abd07b71a14c31e0
maxi74u <maxi74u@protonmail.com>
2023-12-18 01:21:30 +0300

    test-auto-backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,18 +7,18 @@ databaseName=""
 
 function tarZip {
     #tar
-    tar -cvf $( realpath "$backupsPath/$databaseName-$fileName.tar" )  "../databases/$databaseName/" 
+    tar -cvf $( realpath "$backupsPath/$3-$fileName.tar" )  "../databases/$3/" 
 }
 
 function gzip {
     # gzip
-    tar -czvf $( realpath "$backupsPath/$databaseName-$fileName.gz" )  "../databases/$databaseName/"
+    tar -czvf $( realpath "$backupsPath/$3-$fileName.gz" )  "../databases/$3/"
 }
 
 function zipZip {
    # zip
 #    echo "~/../..$backupsPath/$databaseName-$fileName.zip"
-    zip $( realpath "$backupsPath/$databaseName-$fileName.zip") "../databases/$databaseName"
+    zip $( realpath "$backupsPath/$3-$fileName.zip") "../databases/$3"
     exit 0
 }
 
@@ -34,8 +34,7 @@ fi
 
 # user want to do manual backup
 if [ $1 == "-m" ]; then
-    echo "enter database name to backup"
-    read databaseName
+    
     # also add default option if user does not specify second argument    
     if [ $2 == "--tar" ]; then
         tarZip
@@ -47,3 +46,28 @@ if [ $1 == "-m" ]; then
     exit 0;
 fi
 
+HOUR=$(date +"%H")
+DAY=$(date +"%d")
+MONTH=$(date +"%m")
+directory=$( pwd )
+
+if [ $1 == "-s" ]; then
+ # prompt user to enter backup frequency( hourly, daily, monthly, yearly, every 5 minutes)
+ echo "Enter backup frequency (hourly, daily, monthly, yearly, every 5 minutes):"
+ read frequency
+ # also add default option if user does not specify second argument 
+if [ $2 == "--tar" ]; then
+   # add cron job to schedule tar backup
+   if [ $frequency == "hourly" ]; then
+       "0 * * * * $PWD/backup.sh -m --tar $databaseName" | crontab -
+   elif [ $frequency == "daily" ]; then
+       "0 $HOUR * * * $PWD/backup.sh -m --tar $databaseName" | crontab -
+   elif [ $frequency == "monthly" ]; then
+       "0 $HOUR $DAY * * $PWD/backup.sh -m --tar $databaseName" | crontab -
+   elif [ $frequency == "yearly" ]; then
+       "0 $HOUR $DAY $MONTH * $PWD/backup.sh -m --tar $databaseName" | crontab -
+   elif [ $frequency == "every 5 minutes" ]; then
+       "*/5 * * * * $PWD/backup.sh -m --tar $databaseName" | crontab -
+   fi
+fi
+fi
