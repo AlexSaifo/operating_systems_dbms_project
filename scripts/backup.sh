924338733bf0c962115e466424481e0b00876d03
maxi74u <maxi74u@protonmail.com>
2023-12-17 23:34:45 +0300

    manual backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,6 +3,24 @@
 backupsPath="/opt/backups" 
 fileName="backup-$(date +"%Y-%m-%d")"
 
+databaseName=""
+
+function tarZip {
+    #tar
+    tar -cvf $( realpath "$backupsPath/$databaseName-$fileName.tar" )  "../databases/$databaseName/" 
+}
+
+function gzip {
+    # gzip
+    tar -czvf $( realpath "$backupsPath/$databaseName-$fileName.gz" )  "../databases/$databaseName/"
+}
+
+function zip {
+    # zip
+    zip -r $( realpath "$backupsPath/$databaseName-$fileName.zip" )  "../databases/$databaseName/"
+}
+
+# show help for usage
 if [ $1 == "-h" ]; then
     echo "Usage $0 [OPTIONS] [DATABASE_NAME]"
     echo "-"
@@ -10,12 +28,17 @@ if [ $1 == "-h" ]; then
     exit 0
 fi
 
-# echo $backupsPath 'fuck me'
+echo "enter database name to backup"
+    read databaseName
 
-zip -r $( realpath "$backupsPath/$fileName.zip" )  ../databases/
+# user want to do manual backup
+if [ $1 == "-m" ]; then
+    
+    if [ $2 == "--tar"]; then
+        tarZip
+    if [ $2 == "--zip" ]
+        zip
+    if [ $3 == "--gzip"]
+        gzip
+fi
 
-# tar
-tar -cvf $( realpath "$backupsPath/$fileName.tar" )  ../databases
-
-# gzip
-tar -czvf $( realpath "$backupsPath/$fileName.gz" )  ../databases
