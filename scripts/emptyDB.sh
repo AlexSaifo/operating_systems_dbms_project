34b85716327f8487ac39816b72a98676cab2eefd
maxi74u <maxi74u@protonmail.com>
2023-12-18 20:41:20 +0300

    change emptyDB.sh to new Alaa version

--- scripts/emptyDB.sh
+++ scripts/emptyDB.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
-dir="databases"
-
+dir="../databases"
+x=0
 # Loop over all directories in the databases folder
-find ${dir} -mindepth 1 -maxdepth 1 -type d -exec bash -c '
- folder="{}";
- # Check if the current user is the owner of the directory and can delete the directory
- if [ "$(stat -c %U ${folder})" = "$(whoami)" ] && test -w ${folder}
- then
-     # Check if the directory is empty
-     if [ ! -z "$(ls -A ${folder})" ];
-     then
-         # Print the name of the directory
-         echo ${folder}
-     fi
- fi
-' \;
+for folder in $(ls -d -- ${dir}/*/)
+do
+    # Check if the current user can delete the directory
+    if test -w ${folder}
+    then
+        # Check if the directory is empty
+        if [ ! -z "$(ls -A ${folder})" ];
+        then
+            all_files_one_line=true
+
+            for file in "$folder"/*
+            do
+            if [[ -f $file ]]
+            then
+                num_lines=$(wc -l < "$file")
+                if (( num_lines > 1 ))
+                then
+                    all_files_one_line=false
+                    break
+                fi
+            fi
+            done
+            if ! $all_files_one_line
+            then
+                echo ${folder}
+                x=$((x + 1))
+            fi
+        fi
+    fi
+done
+
+if [ $x -eq 0 ]
+then
+    echo "No Databases to be emptied">&2
+    echo "$database_name"
+    exit 1
+fi
 
 read -p "Enter database's name to be deleted: databases/" database_name
 
 # Check if the directory exists and if the user has write permissions to it
-if [ ! -d "databases/${database_name}" ] || [ ! -w "databases/${database_name}" ]|| [ -z $database_name ]
+if [ ! -d "../databases/${database_name}" ] || [ ! -w "../databases/${database_name}" ]|| [ -z $database_name ]
 then
  echo "Invalid Database name">&2
  echo "$database_name"
@@ -27,15 +51,48 @@ then
 fi
 
 # Check if the directory is empty
-if [ -z "$(ls -A "databases/${database_name}")" ];
+if [ -z "$(ls -A "../databases/${database_name}")" ];
 then
  echo "database is empty">&2
  echo "$database_name"
  exit 1
 fi
 
+if [ ! -z "$(ls -A "../databases/${database_name}")" ];
+then
+    all_files_one_line=true
+
+    for file in "../databases/${database_name}"/*
+    do
+    if [[ -f $file ]]
+    then
+        num_lines=$(wc -l < "$file")
+        if (( num_lines > 1 ))
+        then
+            all_files_one_line=false
+            break
+        fi
+    fi
+    done
+    if  $all_files_one_line
+    then
+       echo "Database already empty!">&2
+       echo $database_name
+       exit 1
+    fi
+fi
+
+
+
 # Delete the contents of the directory
-rm -r "databases/${database_name}"/*
+
+for file in "../databases/${database_name}"/*
+do
+  if [[ -f $file ]]
+  then
+      sed -i '2,$d' "$file"
+  fi
+done
 
 echo "database ${database_name} now is empty.">&2
 echo "$database_name"
