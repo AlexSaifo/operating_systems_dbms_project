266de8ef48b70fa4aba5a5607064bf2f41795f41
maxi74u <maxi74u@protonmail.com>
2023-12-18 20:42:14 +0300

    update deleteDB.sh to new Alaa version

--- scripts/deleteDB.sh
+++ scripts/deleteDB.sh
@@ -5,7 +5,7 @@
 # ● The script should show all the databases inside the Database directory to choose one to delete
 
 #!/bin/bash
-dir="databases"
+dir="../databases"
 # if ! test -r "${databases_path}"
 # then
 #    echo "Permission Denied."
@@ -25,7 +25,27 @@ do
             # Print the name of the directory
             x=$((x + 1))
             echo ${folder}
-        elif [ - ]
+        else
+            all_files_one_line=true
+
+            for file in "$folder"/*
+            do
+            if [[ -f $file ]]
+            then
+                num_lines=$(wc -l < "$file")
+                if (( num_lines > 1 ))
+                then
+                    all_files_one_line=false
+                    break
+                fi
+            fi
+            done
+
+            if $all_files_one_line
+            then
+                echo ${folder}
+                x=$((x + 1))
+            fi
         fi
     fi
 done
@@ -38,15 +58,30 @@ fi
 read -p "Enter database's name to be deleted: databases/" database_name
 
 
-if [ ! -d "databases/${database_name}" ] || [ ! -w "databases/${database_name}" ] || [ -z $database_name ]
+if [ ! -d "../databases/${database_name}" ] || [ ! -w "../databases/${database_name}" ] || [ -z $database_name ]
 then
     echo "Invalid Database name">&2
     echo "$database_name"
     exit 1
 fi
 
-# Check if the directory is empty
-if [ ! -z "$(ls -A "databases/${database_name}")" ];
+# Check if the directory is valid
+all_files_one_line=true
+
+for file in "../databases/${database_name}"/*
+do
+if [[ -f $file ]]
+then
+    num_lines=$(wc -l < "$file")
+    if (( num_lines > 1 ))
+    then
+        all_files_one_line=false
+        break
+    fi
+fi
+done
+
+if ! $all_files_one_line
 then
     echo "database is not empty">&2
     echo "$database_name"
@@ -59,7 +94,7 @@ set -e
 sudo groupdel $database_name || exit 1
 
 # Delete the folder
-yes | rm -d "databases/${database_name}" || exit 1
+yes | rm -d -r "../databases/${database_name}" || exit 1
 echo "The database deleted successfully">&2
 echo "$database_name"
 exit 0
